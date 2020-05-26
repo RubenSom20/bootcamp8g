4fdf187d57f6b2d1c78fd148b36acf677b84f254
Ruben Somuano <rubasomuano.g2@gmail.com>
2020-05-26 17:15:47 -0500

    Operadores Aritméticos js

---
+++ s7javascrpt/js/main.js
@@ -0,0 +1,30 @@
+/*  Operadores Aritmeticos*/
+
+var a = 5
+var b = 38
+
+var additionResult = a + b
+
+var multiplicationResult = a * b
+
+var remanierResult = a % b
+
+var divisionResult = a / b
+
+var subtractionResult = a - b
+
+
+console.log("La suma de A más B es igual a" ,additionResult);
+
+console.log("La multiplicación de A por B es igual a" ,multiplicationResult);
+
+console.log("El residuo de A y B es igual a" ,remanierResult);
+
+console.log("La división de A entre B es igual a" ,divisionResult);
+
+console.log("La resta de A menos B es igual a" ,subtractionResult);
+
+
+
+
+
